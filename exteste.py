9c90c25cbdbb0fe5b71c2b46b2cc0263938a79b2
Vitor Renan <vitorrenanalmeida@gmail.com>
2023-08-02 23:55:33 -0300

    Daily Commit

--- exteste.py
+++ exteste.py
@@ -1,4 +1,22 @@
-bill = 50
-n = int(input('\033[1;34mWhat amount do you want to withdraw in dollars? \033[m'))
-if n >= bill:
-    print(n-bill)
+lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim', 'Batata Frita')
+
+for cont in range(0, len(lanche)):
+    print(f'Eu vou comer {lanche[cont]}')
+for comida in lanche:
+    print(f'Eu vou comer {comida[0:2]}')
+for pos, comida in enumerate(lanche):
+    print(f'Eu vou comer {comida} na posição {pos}')
+
+print(sorted(lanche))
+
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = b + a
+print(c)
+print(c.count(5))
+print(c.index(5))
+print(c.index(5, 1))
+
+
+
+
