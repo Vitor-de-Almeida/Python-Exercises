b0347773de8e40826e9e76bb015e5947b7942e2e
Vitor Renan <vitorrenanalmeida@gmail.com>
2023-08-09 18:49:59 -0300

    Daily Commit

--- ex077.py
+++ ex077.py
@@ -1,4 +1,4 @@
-"""sadasd"""
+"""Make a program that have a tuple with random words. After that, you must show vowels for each word"""
 words = ('Learn', 'Programming', 'Language', 'Pythin', 'Course', 'Free', 'Study', 'Practice',
          'Work', 'Market', 'Programmer')
 for word in words:
