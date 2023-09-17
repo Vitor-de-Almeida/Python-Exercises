ebe15a2288a2427fa6cfc2147767ff8b0db4e7ac
Vitor Renan <vitorrenanalmeida@gmail.com>
2023-09-17 19:48:37 -0300

    Daily Commit

--- ex099.py
+++ ex099.py
@@ -5,9 +5,9 @@ Your program has to analyze all the values and say which one is the largest."""
 def bigger(*num):
     big = 0
     print('-=-'*7)
-    print('\033[1;97manalyzing past values\033[m')
+    print('\033[1;97mAnalyzing past values...\033[m')
     for c in range(0, len(num)):
-        print(f'\033[1;36m{num[c]}\033[m', end=' ')
+        print(f'\033[1;36m{num[c]} \033[m', end='')
         if c == 0:
             big = num[c]
         else:
@@ -22,5 +22,5 @@ bigger(2, 9, 4, 5, 7, 1)
 bigger(4, 7, 0)
 bigger(1, 2)
 bigger(6)
-bigger(0)
+bigger()
 
--- ex100.py
+++ ex100.py
@@ -1,3 +1,27 @@
 """Make a program that has a list of called numbers and two functions called sortar() and sumpar().
 The first function will classify 5 numbers and place them in the list and the second function
  will show the sum of all even values drawn by the previous function."""
+from random import randint
+
+
+def sort(lst):
+    print("Sorting the five numbers of the list: ", end='')
+    for c in range(0, 5):
+        lst.append(randint(1, 10))
+        print(f'{lst[c]}', end=' ')
+    print()
+
+
+def sumpair(lst):
+    som = 0
+    for p in lst:
+        if p % 2 == 0:
+            som += p
+    print(f'Adding the even values of {numbers}, we got {som}')
+
+
+numbers = list()
+sort(numbers)
+sumpair(numbers)
+
+
